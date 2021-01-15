bf4902667539432bf8ce213d81c2637bbb0f3507
Siim Tulev <siimtulev@gmail.com>
2021-01-15 22:29:48 +0200

    Loading effect fixed (smoother loading effect), Portfolio under development

--- wwwroot/js/green4.js
+++ wwwroot/js/green4.js
@@ -25,13 +25,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
         // waits til next tick render to run code (prevents running in the middle of render tick)
         window.requestAnimationFrame(function () {
 
-            $(".loader-wrapper").fadeOut("slow");
-            gsap.to('.panel', 1.5, { scaleY: 1, delay: 0.5, height: '100vh', ease: Circ.easeOut });
-            gsap.to('#light', { duration: 5, opacity: 1, y: 0, delay: 0.5, ease: "elastic.out(1, 1)" });
-            gsap.to('p', { duration: 2, delay: 0.5, opacity: 1, y: 20, delay: 1.9 });
-            gsap.to('.box', { duration: 3, delay: 0.5, opacity: 1, scale: 1, ease: "elastic.out(1, 1)" })
-            gsap.from('.side-menu', { duration: 3.5,  x: '100vw', ease: "circ.out" })
+            //$(".loader-wrapper").fadeOut("slow");
+           
 
+            //gsap.to('.panel', 1.5, { scaleY: 1, delay: 0.5, height: '100vh', ease: Circ.easeOut });
+            //gsap.to('#light', { duration: 5, opacity: 1, y: 0, delay: 0.5, ease: "elastic.out(1, 1)" });
+            //gsap.to('p', { duration: 2, delay: 0.5, opacity: 1, y: 20, delay: 1.9 });
+            //gsap.to('.box', { duration: 3, delay: 0.5, opacity: 1, scale: 1, ease: "elastic.out(1, 1)" })
+            //gsap.from('.side-menu', { duration: 3.5,  x: '100vw', ease: "circ.out" })
+            //gsap.from('.EngForTom', { duration: 1, x: '-100vw', stagger: 0.5 })
         });
     };
 });
